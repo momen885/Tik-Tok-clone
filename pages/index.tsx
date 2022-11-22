fa13b4f7fe0bae91afdcd1c3d4daf1aa8b7f63ba
Momen Abdelghafar <momen885@yahoo.com>
2022-11-22 21:39:52 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Next js project with tailwindcss 
+      </h1>
+    </div>
+  )
+}
